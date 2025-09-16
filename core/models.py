15d44412fd5b5894169ad5bcda5ea860f40096ac
peniel <tosalabuzz@gmail.com>
2025-09-16 10:55:56 +0200

    add domain

--- core/models.py
+++ core/models.py
@@ -162,10 +162,10 @@ class Citoyen(models.Model):
         # Numéro unique si absent
         if not self.numero_identite:
             self.numero_identite = str(uuid.uuid4().int)[:12]
-
+        
         # Génération du QR code si absent
         if not self.qr_code:
-            qr_img = qrcode.make(f"https://monsite.com/verifier_qr/?numero={self.numero_identite}")
+            qr_img = qrcode.make(f"https://cartecitoyen.onrender.com/verifier_qr/?numero={self.numero_identite}")
             buffer = BytesIO()
             qr_img.save(buffer, format="PNG")
             filename = f"qr_{self.numero_identite}.png"
