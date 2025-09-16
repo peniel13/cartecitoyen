538375cbdf12d0262019b1721df70bb2bc4c43c7
peniel <tosalabuzz@gmail.com>
2025-09-16 15:26:49 +0200

    delete build

--- cartecitoyen/settings.py
+++ cartecitoyen/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS= [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/'media'
 
