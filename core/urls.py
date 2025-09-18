6e83e39447376327acec9f6a4471258ff709917a
peniel <tosalabuzz@gmail.com>
2025-09-18 14:17:54 +0200

    mis a jour3

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('signup',views.signup, name='signup'),
     path('signin',views.signin, name='signin'),
     path('signout',views.signout, name='signout'),
-    path('profile',views.profile, name='profile'),
     path("profile/<int:user_id>/", views.profile, name="profile"),
     path('update_profile',views.update_profile, name='update_profile'),
     path("citoyens/", views.citoyens_list, name="citoyens_list"),
